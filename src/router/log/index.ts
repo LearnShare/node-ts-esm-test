ad148ca71ab8048bf7955e69d5ee482e6342ba67
LearnShare <learnshare@126.com>
2025-04-17 14:52:23 +0800

    update: build script, fixed: prisma API build error.

--- src/router/log/index.ts
+++ src/router/log/index.ts
@@ -9,7 +9,7 @@ import prisma from '@/lib/prisma';
 const logRouter = Router();
 
 logRouter.get('/', async (req: Request, res: Response) => {
-  const log = await prisma.log.create();
+  const log = await prisma.log.create({});
   res.json(log);
 });
 
